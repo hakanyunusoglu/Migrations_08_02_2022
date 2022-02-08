2ceb72414af475b9b1711e93281544b19a08dbdc
hakanyunusoglu <hakanyunusoglu93@gmail.com>
2022-02-08 10:10:51 +0300

    gfdgdgd

---
+++ CodeFirstWithoutAutoMigration/Entity/DatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirstWithoutAutoMigration.Entity
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base("DatabaseContext")
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ CodeFirstWithoutAutoMigration/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CodeFirstWithoutAutoMigration.MvcApplication" Language="C#" %>
